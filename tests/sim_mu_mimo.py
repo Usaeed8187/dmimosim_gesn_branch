0b58ab5f816212c4e8aaababd744cef1cd2ae6cc
Donald <yiliang@vt.edu>
2024-07-09 11:07:40 -0400

    Add simulation script with sync error

--- tests/sim_mu_mimo.py
+++ tests/sim_mu_mimo.py
@@ -24,9 +24,10 @@ if __name__ == "__main__":
     cfg.total_slots = 25        # total number of slots in ns-3 channels
     cfg.start_slot_idx = 15     # starting slots (must be greater than csi_delay + 5)
     cfg.csi_delay = 4           # feedback delay in number of subframe
-    cfg.num_tx_streams = 12     # 6/8/12 equal to total number of streams
+    cfg.num_tx_streams = 6     # 6/8/12 equal to total number of streams
     cfg.cfo_sigma = 0.0         # in Hz
     cfg.sto_sigma = 0.0         # in nanosecond
+    cfg.ns3_folder = "../ns3/channels/"
 
     # Modulation order: 2/4/6 for QPSK/16QAM/64QAM
     modulation_orders = [2, 4, 6]
---
+++ tests/sim_mu_mimo_sync.py
@@ -0,0 +1,83 @@
+"""
+Simulation of baseline scenario with ns-3 channels
+
+This scripts should be called from the "tests" folder
+"""
+
+# add system folder for the dmimo library
+import sys
+import os
+sys.path.append(os.path.join('..'))
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from dmimo.config import SimConfig
+from dmimo.mu_mimo import sim_mu_mimo_all
+
+
+# Main function
+if __name__ == "__main__":
+
+    # Simulation settings
+    cfg = SimConfig()
+    cfg.total_slots = 25        # total number of slots in ns-3 channels
+    cfg.start_slot_idx = 15     # starting slots (must be greater than csi_delay + 5)
+    cfg.csi_delay = 4           # feedback delay in number of subframe
+    cfg.num_tx_streams = 6      # 6/8/12 equal to total number of streams
+    cfg.cfo_sigma = 0.0         # in Hz
+    cfg.sto_sigma = 0.0         # in nanosecond
+    cfg.ns3_folder = "../ns3/channels/"
+
+    # Modulation order: 2/4/6 for QPSK/16QAM/64QAM
+    modulation_orders = [2, 4, 6]
+    num_modulations = len(modulation_orders)
+    ber = np.zeros((2, num_modulations))
+    ldpc_ber = np.zeros((2, num_modulations))
+    goodput = np.zeros((2, num_modulations))
+    throughput = np.zeros((2, num_modulations))
+
+    for sto in [10, 20]:
+        for cfo in [100, 200, 300]:
+            cfg.sto_sigma = sto
+            cfg.cfo_sigma = cfo
+
+            for k in range(num_modulations):
+                cfg.modulation_order = modulation_orders[k]
+                rst_bd = sim_mu_mimo_all(cfg, precoding_method="BD")
+                ber[0, k] = rst_bd[0]
+                ldpc_ber[0, k] = rst_bd[1]
+                goodput[0, k] = rst_bd[2]
+                throughput[0, k] = rst_bd[3]
+                rst_zf = sim_mu_mimo_all(cfg, precoding_method="ZF")
+                ber[1, k] = rst_zf[0]
+                ldpc_ber[1, k] = rst_zf[1]
+                goodput[1, k] = rst_zf[2]
+                throughput[1, k] = rst_zf[3]
+
+                fig, ax = plt.subplots(1, 3, figsize=(15, 4))
+
+                ax[0].set_title("MU-MIMO")
+                ax[0].set_xlabel('Modulation (bits/symbol)')
+                ax[0].set_ylabel('BER')
+                ax[0].plot(modulation_orders, ber.transpose(), 'o-')
+                ax[0].legend(['BD', 'ZF'])
+
+                ax[1].set_title("MU-MIMO")
+                ax[1].set_xlabel('Modulation (bits/symbol)')
+                ax[1].set_ylabel('Coded BER')
+                ax[1].plot(modulation_orders, ldpc_ber.transpose(), 'd-')
+                ax[1].legend(['BD', 'ZF'])
+
+                ax[2].set_title("MU-MIMO")
+                ax[2].set_xlabel('Modulation (bits/symbol)')
+                ax[2].set_ylabel('Goodput/Throughput (Mbps)')
+                ax[2].plot(modulation_orders, goodput.transpose(), 's-')
+                ax[2].plot(modulation_orders, throughput.transpose(), 'd-')
+                ax[2].legend(['Goodput-BD', 'Goodput-ZF', 'Throughput-BD', 'Throughput-ZF'])
+
+                plt.savefig("../results/mu_mimo_results_cfo{}_sto{}.png".format(cfo, sto))
+
+                np.savez("../results/mu_mimo_results_cfo{}_sto{}.npz".format(cfo, sto),
+                         ber=ber, ldpc_ber=ldpc_ber, goodput=goodput, throughput=throughput)
+
