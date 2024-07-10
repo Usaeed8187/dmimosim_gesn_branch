9ce546532766d51d978267729bc30982589616b2
Donald <yiliang@vt.edu>
2024-07-10 11:29:50 -0400

    Add test for channel prediction

---
+++ dmimo/config/rcconfig.py
@@ -0,0 +1,156 @@
+# dMIMO network scenarios
+
+from .config import Config
+
+
+class RCConfig(Config):
+
+    def __init__(self, **kwargs):
+        self._name = "RC Configuration"
+        self._num_neurons = 16  # 256, 16, 8
+        self._W_tran_sparsity = 0.4  # 0.1, 0.4
+        self._W_tran_radius = 0.5
+        self._input_scale = 0.8 
+        self._initial_forget_length = 0
+        self._max_forget_length = 1
+        self._forget_length_search_step = 1
+        self._window_length = 1
+        self._learning_delay = True
+        self._enable_window = False
+        self._regularization = 1
+        self._type = 'complex' # real, complex
+        self._DF_rls = False
+        self._history_len = 2 # number of subframes that we use to train
+        self._prediction_on = True
+        self._treatment = 'SISO' # SISO, vectorized_MIMO, piece_wise_vectorized_MIMO, TODO: piece_wise_MIMO not currently implemented
+
+
+        super().__init__(**kwargs)
+
+    @property
+    def num_neurons(self):
+        return self._num_neurons
+
+    @num_neurons.setter
+    def num_neurons(self, val):
+        self._num_neurons = val
+
+    @property
+    def W_tran_sparsity(self):
+        return self._W_tran_sparsity
+
+    @W_tran_sparsity.setter
+    def W_tran_sparsity(self, val):
+        self._W_tran_sparsity = val
+
+    @property
+    def W_tran_radius(self):
+        return self._W_tran_radius
+
+    @W_tran_radius.setter
+    def W_tran_radius(self, val):
+        self._W_tran_radius = val
+
+    @property
+    def input_scale(self):
+        return self._input_scale
+
+    @input_scale.setter
+    def input_scale(self, val):
+        self._input_scale = val
+
+    @property
+    def initial_forget_length(self):
+        return self._initial_forget_length
+
+    @initial_forget_length.setter
+    def initial_forget_length(self, val):
+        self._initial_forget_length = val
+
+    @property
+    def max_forget_length(self):
+        return self._max_forget_length
+
+    @max_forget_length.setter
+    def max_forget_length(self, val):
+        self._max_forget_length = val
+
+    @property
+    def forget_length_search_step(self):
+        return self._forget_length_search_step
+
+    @forget_length_search_step.setter
+    def forget_length_search_step(self, val):
+        self._forget_length_search_step = val
+
+    @property
+    def window_length(self):
+        return self._window_length
+
+    @window_length.setter
+    def window_length(self, val):
+        self._window_length = val
+
+    @property
+    def learning_delay(self):
+        return self._learning_delay
+
+    @learning_delay.setter
+    def learning_delay(self, val):
+        self._learning_delay = val
+
+    @property
+    def enable_window(self):
+        return self._enable_window
+
+    @enable_window.setter
+    def enable_window(self, val):
+        self._enable_window = val
+
+    @property
+    def regularization(self):
+        return self._regularization
+
+    @regularization.setter
+    def regularization(self, val):
+        self._regularization = val
+
+    @property
+    def type(self):
+        return self._type
+
+    @type.setter
+    def type(self, val):
+        self._type = val
+
+    @property
+    def DF_rls(self):
+        return self._DF_rls
+
+    @DF_rls.setter
+    def DF_rls(self, val):
+        self._DF_rls = val
+
+    @property
+    def history_len(self):
+        return self._history_len
+
+    @history_len.setter
+    def history_len(self, val):
+        self._history_len = val
+    
+    @property
+    def prediction_on(self):
+        return self._prediction_on
+
+    @prediction_on.setter
+    def prediction_on(self, val):
+        self._prediction_on = val
+    
+    @property
+    def treatment(self):
+        return self._treatment
+
+    @treatment.setter
+    def treatment(self, val):
+        self._treatment = val
---
+++ tests/sim_su_mimo_chanpred.py
@@ -0,0 +1,80 @@
+"""
+Simulation of SU-MIMO scenario with ns-3 channels
+
+This scripts should be called from the "tests" folder
+"""
+
+# add system folder for the dmimo library
+import sys
+import os
+sys.path.append(os.path.join('..'))
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from dmimo.config import SimConfig
+from dmimo.su_mimo import sim_su_mimo_all
+
+
+# Main function
+if __name__ == "__main__":
+
+    # Simulation settings
+    cfg = SimConfig()
+    cfg.total_slots = 20        # total number of slots in ns-3 channels
+    cfg.start_slot_idx = 16     # starting slots (must be greater than csi_delay + 5)
+    cfg.csi_delay = 6           # feedback delay in number of subframe
+    cfg.num_tx_streams = 6      # 4/6 equal to total number of streams
+    cfg.cfo_sigma = 0.0         # in Hz
+    cfg.sto_sigma = 0.0         # in nanosecond
+    cfg.ns3_folder = "../ns3/channels4pred/"
+
+    # Modulation order: 2/4/6 for QPSK/16QAM/64QAM
+    modulation_orders = [2, 4, 6]
+    num_modulations = len(modulation_orders)
+    ber = np.zeros((2, num_modulations))
+    ldpc_ber = np.zeros((2, num_modulations))
+    goodput = np.zeros((2, num_modulations))
+    throughput = np.zeros((2, num_modulations))
+
+    for k in range(num_modulations):
+        cfg.modulation_order = modulation_orders[k]
+        cfg.csi_prediction = True
+        rst_zf = sim_su_mimo_all(cfg, precoding_method="ZF")
+        ber[1, k] = rst_zf[0]
+        ldpc_ber[1, k] = rst_zf[1]
+        goodput[1, k] = rst_zf[2]
+        throughput[1, k] = rst_zf[3]
+        cfg.csi_prediction = False
+        rst_svd = sim_su_mimo_all(cfg, precoding_method="ZF")
+        ber[0, k] = rst_svd[0]
+        ldpc_ber[0, k] = rst_svd[1]
+        goodput[0, k] = rst_svd[2]
+        throughput[0, k] = rst_svd[3]
+
+    fig, ax = plt.subplots(1, 3, figsize=(15, 4))
+
+    ax[0].set_title("SU-MIMO")
+    ax[0].set_xlabel('Modulation (bits/symbol)')
+    ax[0].set_ylabel('BER')
+    ax[0].plot(modulation_orders, ber.transpose(), 'o-')
+    ax[0].legend(['Estimation', 'Prediction'])
+
+    ax[1].set_title("SU-MIMO")
+    ax[1].set_xlabel('Modulation (bits/symbol)')
+    ax[1].set_ylabel('Coded BER')
+    ax[1].plot(modulation_orders, ldpc_ber.transpose(), 'd-')
+    ax[1].legend(['Estimation', 'Prediction'])
+
+    ax[2].set_title("SU-MIMO")
+    ax[2].set_xlabel('Modulation (bits/symbol)')
+    ax[2].set_ylabel('Goodput/Throughput (Mbps)')
+    ax[2].plot(modulation_orders, goodput.transpose(), 's-')
+    ax[2].plot(modulation_orders, throughput.transpose(), 'd-')
+    ax[2].legend(['Goodput', 'Goodput-Pred', 'Throughput', 'Throughput-Pred'])
+
+    plt.savefig("../results/su_mimo_results_chanpred.png")
+
+    np.savez("../results/su_mimo_results_chanpred.npz",
+             ber=ber, ldpc_ber=ldpc_ber, goodput=goodput, throughput=throughput)
+
