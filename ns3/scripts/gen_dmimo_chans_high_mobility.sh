13b2726c831490e1c3695d73af85054c6cebeb19
Donald Liang <yiliang@vt.edu>
2024-09-10 13:18:21 -0400

    Add ns-3 channel generation bash scripts

---
+++ ns3/scripts/gen_dmimo_chans_high_mobility.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python main.py --seed 3007 --scenario V2V-Urban --small_scale_fading --num_subframes 100 --squad1_speed_km_h=10.0 --squad2_speed_km_h=10.0 --intra_sq1_rw_speed_km_h=1.0 --intra_sq2_rw_speed_km_h=1.0 --buildings_file 1narrow.txt 
+
---
+++ ns3/scripts/gen_dmimo_chans_medium_mobility.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python main.py --seed 3007 --scenario V2V-Urban --small_scale_fading --num_subframes 100 --squad1_speed_km_h=3.0 --squad2_speed_km_h=3.0 --intra_sq1_rw_speed_km_h=0.3 --intra_sq2_rw_speed_km_h=0.3 --buildings_file 1narrow.txt 
+
