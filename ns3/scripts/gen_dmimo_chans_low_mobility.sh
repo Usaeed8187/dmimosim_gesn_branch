6f49e682214ffacc41d6ba99f2b13982160cbd10
Usama <usaeed8187@vt.edu>
2024-09-10 13:23:20 -0400

    channel generation script settings update

--- ns3/scripts/gen_dmimo_chans_low_mobility.sh
+++ ns3/scripts/gen_dmimo_chans_low_mobility.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python main.py --seed 3007 --scenario V2V-Urban --small_scale_fading --num_subframes 100 --squad1_speed_km_h=0 --squad2_speed_km_h=0 --intra_sq1_rw_speed_km_h=0.1 --intra_sq2_rw_speed_km_h=0.1 --buildings_file 1narrow.txt 
+python main.py --seed 3007 --scenario V2V-Urban --small_scale_fading --num_subframes 100 --squad1_speed_km_h=0.1 --squad2_speed_km_h=0.1 --intra_sq1_rw_speed_km_h=0.01 --intra_sq2_rw_speed_km_h=0.01 --buildings_file 1narrow.txt  --squad1_direction=0 --squad2_direction=0.785398
 
