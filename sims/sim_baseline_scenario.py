0af8b9580e80978741693fdc9d640b7848548bce
Donald Liang <yiliang@vt.edu>
2024-09-14 13:33:32 -0400

    Disable Baseline rank/link adaptation by default

--- sims/sim_baseline_scenario.py
+++ sims/sim_baseline_scenario.py
@@ -40,6 +40,8 @@ if __name__ == "__main__":
     cfg.total_slots = 35        # total number of slots in ns-3 channels
     cfg.start_slot_idx = 15     # starting slots (must be greater than csi_delay + 5)
     cfg.csi_delay = 2           # feedback delay in number of subframe
+    cfg.rank_adapt = False      # disable rank adaptation
+    cfg.link_adapt = False      # disable link adaptation
     cfg.cfo_sigma = 0.0         # in Hz
     cfg.sto_sigma = 0.0         # in nanosecond
     cfg.ns3_folder = "../ns3/channels/"
