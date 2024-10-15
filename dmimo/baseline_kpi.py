9f6ef1082e4615bc0c529289c17ba58200924307
Usama <usaeed8187@vt.edu>
2024-10-15 15:34:12 -0400

    solving conflicts

--- dmimo/baseline_kpi.py
+++ dmimo/baseline_kpi.py
@@ -174,23 +174,16 @@ class Baseline(Model):
             return h_freq_csi, rx_snr_db
 
         # Generate PMI feedback and reconstruct the channel at the gNB (based on PMI and CQI, CQI taken from link adaptation)
-<<<<<<< HEAD
-        generate_CSI_feedback = quantized_CSI_feedback(method='5G', num_tx_streams=self.cfg.num_tx_streams, architecture='baseline', snrdb=rx_snr_db)
-        [PMI, rate_for_selected_precoder, precoding_matrices] = generate_CSI_feedback(h_freq_csi)
-        h_freq_csi_reconstructed = generate_CSI_feedback.reconstruct_channel(precoding_matrices, self.cfg.snr_assumed, self.cfg.n_var, self.cfg.bs_txpwr_dbm)
-=======
         if self.cfg._CSI_feedback_method =='5G':
             generate_CSI_feedback = quantized_CSI_feedback(method='5G', num_tx_streams=self.cfg.num_tx_streams, architecture='baseline', snrdb=rx_snr_db)
-            [PMI, rate_for_selected_precoder, precoding_matrix] = generate_CSI_feedback(h_freq_csi)
-            h_freq_csi_reconstructed = generate_CSI_feedback.reconstruct_channel(precoding_matrix, self.cfg.snr_assumed, self.cfg.n_var, self.cfg.bs_txpwr_dbm)
-        
+            [PMI, rate_for_selected_precoder, precoding_matrices] = generate_CSI_feedback(h_freq_csi)
+            h_freq_csi_reconstructed = generate_CSI_feedback.reconstruct_channel(precoding_matrices, self.cfg.snr_assumed, self.cfg.n_var, self.cfg.bs_txpwr_dbm)  
             
         #RVQ is not used for baseline simulations it is only added for simple debugging. 
         elif self.cfg._CSI_feedback_method =='RVQ':
             generate_CSI_feedback = quantized_CSI_feedback(method='RVQ', num_tx_streams=self.cfg.num_tx_streams, architecture='baseline', snrdb=rx_snr_db,total_bits=4,VectorLength=h_freq_csi.shape[4]*2)
             CSI_feedback_report = generate_CSI_feedback(h_freq_csi) 
             Reconstructed_Channel = generate_CSI_feedback.Reconstruction(CSI_feedback_report)
->>>>>>> 7111becd7c03b29d7129cede65c47e947e030e7f
 
         # apply precoding to OFDM grids
         if self.cfg.precoding_method == "ZF":
